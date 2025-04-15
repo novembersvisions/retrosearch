9395ec5df400c0960390aa4fb5c424f6d14c018c
Hamid Rezaee <hr328@cornell.edu>
2025-04-15 00:27:53 -0400

    fixed numpy error

--- backend/app.py
+++ backend/app.py
@@ -183,7 +183,7 @@ def convert_to_serializable(obj):
     if isinstance(obj, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64,
                          np.uint8, np.uint16, np.uint32, np.uint64)):
         return int(obj)
-    elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+    elif isinstance(obj, np.floating):
         return float(obj)
     elif isinstance(obj, np.ndarray):
         return obj.tolist()
